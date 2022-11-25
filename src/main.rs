4656eb2158f393680924c4278df347234ab84541
puripuri2100 <puripuri2100@gmail.com>
2022-11-25 20:32:07 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use anyhow::Result;
+use clap::Parser;
+use quick_xml::Reader;
+use tokio;
+use tokio::fs::*;
+use tokio::io::{AsyncWriteExt, BufReader};
+use tokio_stream::StreamExt;
+use tracing::*;
+use tracing_subscriber;
+use std::path::Path;
+
+mod search;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+  /// 解析結果を出力するJSONファイルへのpath
+  #[clap(short, long)]
+  output: String,
+  /// 法令XMLファイル群が置かれている作業ディレクトリへのpath
+  #[clap(short, long)]
+  work: String,
+  /// 法令ファイルのインデックス情報が書かれたJSONファイルへのpath
+  #[clap(short, long)]
+  index_file: String,
+  /// 検索する単語
+  #[clap(short, long)]
+  search_word: String,
+}
+
+async fn init_logger() -> Result<()> {
+  let subscriber = tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::INFO)
+    .finish();
+  tracing::subscriber::set_global_default(subscriber)?;
+  Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let args = Args::parse();
+
+  init_logger().await?;
+
+  info!("[START] get law data: {:?}", &args.index_file);
+  let raw_data_lst = search::get_law_files_index(&args.index_file).await?;
+  info!("[END] get law data: {:?}", &args.index_file);
+
+  let mut output_file = File::create(&args.output).await?;
+  info!("[START] write json file");
+  output_file.write_all("[".as_bytes()).await?;
+
+  let mut law_data_stream = tokio_stream::iter(raw_data_lst);
+
+  let mut is_head = true;
+
+  let work_dir_path = Path::new(&args.work);
+
+  while let Some(law_data) = law_data_stream.next().await {
+    let file_path = work_dir_path.join(law_data.file);
+    info!("[START] work file: {:?}", file_path);
+    let mut reader = Reader::from_reader(BufReader::new(File::open(&file_path).await?));
+    let chapter_data = search::search_xml(&args.search_word, &mut reader).await?;
+    if !chapter_data.chapter_data.is_empty() {
+      let chapter_data_lst_json_str = serde_json::to_string(&chapter_data)?;
+      info!("[END] work file: {:?}", file_path);
+      info!("[START] data write: {:?}", file_path);
+      if is_head {
+        output_file.write_all("\n".as_bytes()).await?;
+        is_head = false;
+      } else {
+        output_file.write_all(",\n".as_bytes()).await?;
+      }
+      output_file
+        .write_all(chapter_data_lst_json_str.as_bytes())
+        .await?;
+    }
+    info!("[END] data write: {:?}", file_path);
+  }
+
+  output_file.write_all("\n]".as_bytes()).await?;
+  info!("[END write json file");
+  output_file.flush().await?;
+
+  Ok(())
+}
---
+++ src/search.rs
@@ -0,0 +1,486 @@
+use anyhow::Result;
+use encoding_rs::Encoding;
+use quick_xml::{encoding, events::Event, Reader};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use tokio::io::AsyncReadExt;
+use tokio::{fs::File, io::BufReader};
+use tracing::*;
+
+/// 元号
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+pub enum Era {
+  Meiji,
+  Taisho,
+  Showa,
+  Heisei,
+  Reiwa,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+pub struct Date {
+  pub era: Era,
+  pub year: u16,
+  pub month: Option<u8>,
+  pub day: Option<u8>,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+pub struct LawData {
+  pub date: Date,
+  pub file: String,
+  pub name: String,
+  pub num: String,
+  pub id: String,
+}
+
+pub async fn get_law_files_index(index_file_path: &str) -> Result<Vec<LawData>> {
+  let mut f = File::open(index_file_path).await?;
+  let mut buf = Vec::new();
+  f.read_to_end(&mut buf).await?;
+  let file_str = std::str::from_utf8(&buf)?;
+  let raw_data_lst = serde_json::from_str(&file_str)?;
+  Ok(raw_data_lst)
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+pub struct LawParagraph {
+  /// 法令番号
+  pub num: String,
+  /// 見出しと章番号
+  pub chapter_data: Vec<Chapter>,
+}
+
+/// 章・節などを表す
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Default, Deserialize, Serialize)]
+pub struct Chapter {
+  /// 編
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub part: Option<usize>,
+  /// 章
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub chapter: Option<usize>,
+  /// 節
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub section: Option<usize>,
+  /// 款
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub subsection: Option<usize>,
+  /// 目
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub division: Option<usize>,
+  /// 条
+  pub article: String,
+  /// 項
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub paragraph: Option<String>,
+  /// 号
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub item: Option<String>,
+  /// イロハなど（深さも記録する）
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub sub_item: Option<(usize, String)>,
+  /// 附則の場合につける
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub suppl_provision_title: Option<String>,
+}
+
+/// 指定された単語が含まれる条があったとき、その条番号等のデータのみを保存する。
+/// 後でこのデータをもとに実際の条文を再度取得するのに使いたい。
+pub async fn search_xml(
+  search_str: &str,
+  reader: &mut Reader<BufReader<File>>,
+) -> Result<LawParagraph> {
+  let utf8 = Encoding::for_label(b"utf-8").unwrap();
+
+  let mut lst = vec![];
+  let mut buf = Vec::new();
+  let mut chapter_num = Chapter::default();
+  let mut law_num = String::new();
+  let mut is_law_num_mode = false;
+
+  reader.trim_text(true);
+  loop {
+    match reader.read_event_into_async(&mut buf).await {
+      Ok(Event::Start(tag)) => match tag.name().as_ref() {
+        b"LawNum" => is_law_num_mode = true,
+        b"Part" => {
+          chapter_num = Chapter {
+            part: {
+              match chapter_num.part {
+                Some(n) => Some(n + 1),
+                None => Some(1),
+              }
+            },
+            chapter: None,
+            section: None,
+            subsection: None,
+            division: None,
+            article: chapter_num.article,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          };
+        }
+        b"Chapter" => {
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: {
+              match chapter_num.chapter {
+                Some(n) => Some(n + 1),
+                None => Some(1),
+              }
+            },
+            section: None,
+            subsection: None,
+            division: None,
+            article: chapter_num.article,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"Section" => {
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: {
+              match chapter_num.section {
+                Some(n) => Some(n + 1),
+                None => Some(1),
+              }
+            },
+            subsection: None,
+            division: None,
+            article: chapter_num.article,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"Subsection" => {
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: {
+              match chapter_num.subsection {
+                Some(n) => Some(n + 1),
+                None => Some(1),
+              }
+            },
+            division: None,
+            article: chapter_num.article,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"Division" => {
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: {
+              match chapter_num.division {
+                Some(n) => Some(n + 1),
+                None => Some(1),
+              }
+            },
+            article: chapter_num.article,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"Article" => {
+          let article_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: article_num_str,
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          };
+          info!("law_num: {}", &law_num);
+          info!("law_chapter: {:?}", &chapter_num);
+        }
+        b"Paragraph" => {
+          let paragraph_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: Some(paragraph_num_str),
+            item: None,
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"Item" => {
+          let item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: Some(item_num_str),
+            sub_item: None,
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem1" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((1, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem2" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((2, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem3" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((3, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem4" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((4, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem5" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((5, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem6" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((6, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        b"SubItem7" => {
+          let sub_item_num_str = tag
+            .attributes()
+            .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+            .map(|res| {
+              encoding::decode(&res.unwrap().value, utf8)
+                .unwrap()
+                .to_string()
+            })
+            .unwrap();
+          chapter_num = Chapter {
+            part: chapter_num.part,
+            chapter: chapter_num.chapter,
+            section: chapter_num.section,
+            subsection: chapter_num.subsection,
+            division: chapter_num.division,
+            article: chapter_num.article,
+            paragraph: chapter_num.paragraph,
+            item: chapter_num.item,
+            sub_item: Some((7, sub_item_num_str)),
+            suppl_provision_title: chapter_num.suppl_provision_title,
+          }
+        }
+        // 附則
+        b"SupplProvision" => {
+          chapter_num = Chapter {
+            part: None,
+            chapter: None,
+            section: None,
+            subsection: None,
+            division: None,
+            article: String::new(),
+            paragraph: None,
+            item: None,
+            sub_item: None,
+            suppl_provision_title: tag
+              .attributes()
+              .find(|res| encoding::decode(res.as_ref().unwrap().key.0, utf8).unwrap() == "Num")
+              .map(|res| {
+                encoding::decode(&res.unwrap().value, utf8)
+                  .unwrap()
+                  .to_string()
+              }),
+          }
+        }
+        _ => (),
+      },
+      Ok(Event::End(tag)) => {
+        if let b"LawNum" = tag.name().as_ref() {
+          is_law_num_mode = false
+        }
+      }
+      Ok(Event::Text(text)) => {
+        if is_law_num_mode {
+          law_num = encoding::decode(&text.into_inner(), utf8)?.to_string();
+        } else {
+          let text_str = encoding::decode(&text.into_inner(), utf8)?.to_string();
+          let is_use_junyou = text_str.contains(search_str);
+          info!("law_num: {}", &law_num);
+          if is_use_junyou {
+            lst.push(chapter_num.clone())
+          }
+        }
+      }
+      Ok(Event::Eof) => break,
+      Err(e) => panic!("法令名APIの結果のXMLの解析中のエラー: {}", e),
+      _ => (),
+    }
+  }
+  lst.sort();
+  lst.dedup();
+  Ok(LawParagraph {
+    num: law_num,
+    chapter_data: lst,
+  })
+}
